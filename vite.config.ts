b64ac22e712770d34b90f509aa92845d77738fcb
陳思瑜 Judy <laplus.space@gmail.com>
2026-03-21 20:20:35 +0800

    chore: 設定 analyzer 以及 sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,43 @@
 import { resolve } from "node:path";
 import typia from "@typia/unplugin/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 import checker from "vite-plugin-checker";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
-	plugins: [
-		typia(),
-		dts({
-			rollupTypes: true,
-			tsconfigPath: "./tsconfig.json",
-			exclude: ["**/*.test.ts", "**/*.spec.ts", "tests", "__tests__"],
-		}),
-		checker({
-			biome: {
-				command: "check",
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [
+			typia(),
+			dts({
+				rollupTypes: true,
+				tsconfigPath: "./tsconfig.json",
+				exclude: ["**/*.test.ts", "**/*.spec.ts", "tests", "__tests__"],
+			}),
+			checker({
+				biome: {
+					command: "check",
+				},
+			}),
+			analyzer({
+				enabled: env.DEBUG === "true",
+				openAnalyzer: env.NODE_ENV === "development",
+				analyzerMode: "static",
+				fileName: "report",
+			}),
+		],
+		build: {
+			sourcemap: true,
+			lib: {
+				entry: resolve(__dirname, "src/main.ts"),
+				name: "sports-radar-crawler",
+				fileName: "index",
 			},
-		}),
-	],
-	build: {
-		lib: {
-			entry: resolve(__dirname, "src/main.ts"),
-			name: "sports-radar-crawler",
-			fileName: "index",
+			rollupOptions: {
+				external: ["ts-results"],
+			},
 		},
-	},
+	};
 });
