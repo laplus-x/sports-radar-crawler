3c28aeaf3cbffa6e72396d18a54f20799f8fd7ab
陳思瑜 Judy <laplus.space@gmail.com>
2026-03-21 21:13:29 +0800

    revert: 修正 vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,18 +2,20 @@ import { resolve } from "node:path";
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		resolve: {
-			alias: {
-				"@": resolve(__dirname, "./src"), // Example alias mapping @ to ./src
+export default defineConfig((env) =>
+	mergeConfig(
+		viteConfig(env),
+		defineConfig({
+			resolve: {
+				alias: {
+					"@": resolve(__dirname, "./src"), // Example alias mapping @ to ./src
+				},
 			},
-		},
-		test: {
-			globals: true,
-			include: ["./src/**/*.{test,spec}.{js,ts}{,x}"],
-			// setupFiles: ["./vitest.setup.ts"],
-		},
-	}),
+			test: {
+				globals: true,
+				include: ["./src/**/*.{test,spec}.{js,ts}{,x}"],
+				// setupFiles: ["./vitest.setup.ts"],
+			},
+		}),
+	),
 );
